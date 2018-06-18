0a4619332016349614831c968c10566b51edd70c
Lynexis7 <alpha_wolf09@hotmail.com>
2018-06-18 16:08:05 -0500

    Examen Final

---
+++ Examen/ExamenFinal/ProyectoFinal/ProyectoFinal/IngresoViewController.designer.cs
@@ -0,0 +1,50 @@
+// WARNING
+//
+// This file has been generated automatically by Visual Studio to store outlets and
+// actions made in the UI designer. If it is removed, they will be lost.
+// Manual changes to this file may not be handled correctly.
+//
+using Foundation;
+using System.CodeDom.Compiler;
+
+namespace ProyectoFinal
+{
+	[Register ("IngresoViewController")]
+	partial class IngresoViewController
+	{
+		[Outlet]
+		UIKit.UIButton btnCancelar { get; set; }
+
+		[Outlet]
+		UIKit.UIButton btnGuardarIngreso { get; set; }
+
+		[Outlet]
+		UIKit.UITextField txtDescripcion { get; set; }
+
+		[Outlet]
+		UIKit.UITextField txtMonto { get; set; }
+		
+		void ReleaseDesignerOutlets ()
+		{
+			if (btnGuardarIngreso != null) {
+				btnGuardarIngreso.Dispose ();
+				btnGuardarIngreso = null;
+			}
+
+			if (btnCancelar != null) {
+				btnCancelar.Dispose ();
+				btnCancelar = null;
+			}
+
+			if (txtMonto != null) {
+				txtMonto.Dispose ();
+				txtMonto = null;
+			}
+
+			if (txtDescripcion != null) {
+				txtDescripcion.Dispose ();
+				txtDescripcion = null;
+			}
+		}
+	}
+}
---
+++ Examen/ExamenFinal/ProyectoFinal/ProyectoFinal/ViewController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+using UIKit;
+
+namespace ProyectoFinal
+{
+    public partial class ViewController : UIViewController
+    {
+        protected ViewController(IntPtr handle) : base(handle)
+        {
+            // Note: this .ctor should not contain any initialization logic.
+        }
+
+        public ViewController()
+        {
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+            // Perform any additional setup after loading the view, typically from a nib.
+        }
+
+        public override void DidReceiveMemoryWarning()
+        {
+            base.DidReceiveMemoryWarning();
+            // Release any cached data, images, etc that aren't in use.
+        }
+    }
+}
